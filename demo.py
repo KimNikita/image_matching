30c9bbf28870290958b67442f677a5818cf09d3b
KimNikita <ghj710@mail.ru>
2022-12-17 21:44:10 +0300

    fix locate_all

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+# TODO fix save path for screenshot
+
 
 def by_image(self, image, accuracy=0.95, second_try=False, similarity=4):
     '''
@@ -22,7 +24,7 @@ def by_image(self, image, accuracy=0.95, second_try=False, similarity=4):
     from PIL import ImageGrab
 
     screenshot = ImageGrab.grab(None)
-    screenshot.save(r'C:/Users/Никита/Desktop/screen.png')
+    screenshot.save('screen.png')
     screenshot = cv.imread('screen.png', 0)
 
     if isinstance(image, str):
@@ -111,6 +113,91 @@ def by_image(self, image, accuracy=0.95, second_try=False, similarity=4):
     plt.show()
 
 
+def locate_one(image, accuracy=0.95, second_try=False, similarity=4):
+    '''
+    image - path to original image of control.\n
+    accuracy - the percentage of pixels matching the original image and the one found on the screen. Default 0.95.\n
+    second_try - indicates whether to use a different search method based on the similarity score.
+    Useful when the size of original image has been changed. Default False.\n
+    similarity - an integer on a five-point scale, sets the minimum similarity of the original image and the one found on the screen.
+    Used with second_try=True. Default 4.\n
+    Second search method works better in cases when the size of the original image has been increased.
+    Function returns rectangle coordinates (upper_left_x, upper_left_y, lower_right_x, lower_right_y) of found control.
+    Size of rectangle are equal to original image size.
+    '''
+    import cv2 as cv
+    import numpy as np
+    from PIL import ImageGrab
+
+    screenshot = ImageGrab.grab(None)
+    screenshot.save('screen.png')
+    screenshot = cv.imread('screen.png', 0)
+
+    if isinstance(image, str):
+        template = cv.imread(image, cv.IMREAD_GRAYSCALE)
+        if template is None:
+            print('cannot read image')
+    else:
+        print('invalid format of image')
+
+    # Apply template Matching
+    res = cv.matchTemplate(screenshot, template, cv.TM_CCOEFF_NORMED)
+
+    if similarity <= 0:
+        similarity = 1
+    similarity = similarity/100-0.001
+
+    a, max_value, b, max_location = cv.minMaxLoc(res)
+    w, h = template.shape[::-1]
+
+    # DEBUG
+    x = max_location[0] + w//2
+    y = max_location[1] + h//2
+    print(x, y, max_value)
+
+    if max_value >= accuracy:
+        box = (max_location[0], max_location[1],
+               max_location[0] + w, max_location[1] + h)
+
+        # DEBUG
+        cv.rectangle(screenshot, max_location,
+                     (max_location[0] + w, max_location[1] + h), (0, 0, 255), 3)
+
+    elif second_try:
+        # Getting each value in format 0.00
+        val00 = round(res[max_location[1]-1][max_location[0]-1], 2)
+        val01 = round(res[max_location[1]][max_location[0]-1], 2)
+        val02 = round(res[max_location[1]+1][max_location[0]-1], 2)
+
+        val10 = round(res[max_location[1]-1][max_location[0]], 2)
+        val12 = round(res[max_location[1]+1][max_location[0]], 2)
+
+        val20 = round(res[max_location[1]-1][max_location[0]+1], 2)
+        val21 = round(res[max_location[1]][max_location[0]+1], 2)
+        val22 = round(res[max_location[1]+1][max_location[0]+1], 2)
+
+        # Make sure that vertical and horizontal values way bigger than in the corners
+        if abs(val01-val00)+abs(val01-val02) >= similarity and abs(val10-val00)+abs(val10-val20) >= similarity \
+                and abs(val21-val20)+abs(val21-val22) >= similarity and abs(val12-val02)+abs(val12-val22) >= similarity:
+
+            box = (max_location[0], max_location[1],
+                   max_location[0] + w, max_location[1] + h)
+
+            # DEBUG
+            cv.rectangle(screenshot, max_location,
+                         (max_location[0] + w, max_location[1] + h), (0, 0, 255), 3)
+
+    # DEBUG
+    from matplotlib import pyplot as plt
+    plt.subplot(121), plt.imshow(res, cmap='gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(screenshot)
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    return box
+
+
 def locate_all(image, count, accuracy=0.95, second_try=False, similarity=4):
     '''
     image - path to original image of control.\n
@@ -129,7 +216,7 @@ def locate_all(image, count, accuracy=0.95, second_try=False, similarity=4):
     from PIL import ImageGrab
 
     screenshot = ImageGrab.grab(None)
-    screenshot.save(r'C:/Users/Никита/Desktop/screen.png')
+    screenshot.save('screen.png')
     screenshot = cv.imread('screen.png', 0)
 
     if isinstance(image, str):
@@ -139,17 +226,17 @@ def locate_all(image, count, accuracy=0.95, second_try=False, similarity=4):
     else:
         print('invalid format of image')
 
-    # Apply template Matching
-    res = cv.matchTemplate(screenshot, template, cv.TM_CCOEFF_NORMED)
-
     if similarity <= 0:
         similarity = 1
     similarity = similarity/100-0.001
 
+    height, width = screenshot.shape
+    w, h = template.shape[::-1]
     boxes = []
     for i in range(0, count):
+        # Apply template Matching
+        res = cv.matchTemplate(screenshot, template, cv.TM_CCOEFF_NORMED)
         a, max_value, b, max_location = cv.minMaxLoc(res)
-        w, h = template.shape[::-1]
 
         # DEBUG
         x = max_location[0] + w//2
@@ -159,11 +246,6 @@ def locate_all(image, count, accuracy=0.95, second_try=False, similarity=4):
         if max_value >= accuracy:
             boxes.append(
                 (max_location[0], max_location[1], max_location[0] + w, max_location[1] + h))
-
-            # DEBUG
-            cv.rectangle(screenshot, max_location,
-                         (max_location[0] + w, max_location[1] + h), (0, 0, 255), 3)
-
         elif second_try:
             # Getting each value in format 0.00
             val00 = round(res[max_location[1]-1][max_location[0]-1], 2)
@@ -184,20 +266,15 @@ def locate_all(image, count, accuracy=0.95, second_try=False, similarity=4):
                 boxes.append(
                     (max_location[0], max_location[1], max_location[0] + w, max_location[1] + h))
 
-                # DEBUG
-                cv.rectangle(screenshot, max_location,
-                             (max_location[0] + w, max_location[1] + h), (0, 0, 255), 3)
-
-        height, width = res.shape
-
-        for i in range(-h//2, h//2+1):
-            if 0 <= max_location[1]+i < height:
-                res[max_location[1]+i][max_location[0]] = 0
-        for j in range(-w//2, w//+1):
-            if 0 <= max_location[0]+j < width:
-                res[max_location[1]][max_location[0]+j] = 0
+        for i in range(0, h):
+            for j in range(0, w):
+                if 0 <= max_location[1]+i < height and 0 <= max_location[0]+j < width:
+                    screenshot[max_location[1]+i][max_location[0]+j] = 0
 
     # DEBUG
+    for box in boxes:
+        cv.rectangle(screenshot, (box[0]+5, box[1]+5),
+                     (box[2]-5, box[3]-5), (255, 255, 255), 3)
     from matplotlib import pyplot as plt
     plt.subplot(121), plt.imshow(res, cmap='gray')
     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
@@ -209,8 +286,9 @@ def locate_all(image, count, accuracy=0.95, second_try=False, similarity=4):
 
 
 def main():
-    #by_image(None, 'original.png', second_try=True)
-    result = locate_all('original.png', 1, second_try=True)
+    # by_image(None, 'original.png', second_try=True)
+    # result = locate_one('original.png', second_try=True)
+    result = locate_all('original.png', 10, second_try=True)
     print(result)
 
 
